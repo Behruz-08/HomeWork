447879a9a32157264b2710e6db462a4d6edfe22e
Бехруз Зоиров <C:\Users\behruz-z\AppData\Roaming\The Bat!>
2024-03-04 17:21:38 +0500

    новые изменения

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,17 +3,52 @@ import './App.css'
 import CarouselImage from './components/Carousel/CarouselImage'
 import LoginForm from './components/Logins/LoginForm'
 import PostList from './components/Post/PostList'
-
 import ProductsCard from './components/Products/ProductsCard'
+
+const productPosts =()=>{
+  return[
+    {
+      content:'это контент',
+      title:'это тайтл'
+    }
+ 
+  ]
+}
+const getPosts = productPosts();
+
+const productsInfo =()=>{
+  return[
+    {
+      id:1,
+      info:"чёрная краска",
+      price:'200c'
+    },
+    {
+      id:2,
+      info:",белая краска",
+      price:'300c'
+    },
+    {
+      id:3,
+      info:"жёлтая краска",
+      price:'400c'
+    }
+ 
+  ]
+}
+
+const getProducts=productsInfo()
 
 
 function App() {
+
+
   
 
   return (
     <>
-    <ProductsCard/>
-    <PostList/>
+    <PostList getPosts={getPosts} />
+    <ProductsCard  productInfo={getProducts}/>
     <LoginForm/>
     <CarouselImage/>
     </>
